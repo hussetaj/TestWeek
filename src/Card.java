d84252ebcbecd12e3b49ac7524567457eacc1d77
hussetaj <hussetaj@sheridancollege.ca>
2026-02-05 12:43:22 -0500

    Initial commit of project

---
+++ src/Card.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class Card {
+    private int value;
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+}
